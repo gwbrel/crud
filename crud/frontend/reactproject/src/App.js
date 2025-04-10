4fc4d469666d39cb38873b8c92fee0764dd63dcb
Gabriel Luis <131543348+gwbrel@users.noreply.github.com>
2025-04-09 21:21:13 -0300

    Update App.js

--- crud/frontend/reactproject/src/App.js
+++ crud/frontend/reactproject/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UsersList from "./UsersList";
 import CreateUser from "./CreateUser";
 import EditUser from "./EditUser";
-import UserDetails from "./UserDetails"; // 1. Importe o novo componente
+import UserDetails from "./UserDetails";
 
 function App() {
     return (
@@ -11,7 +11,7 @@ function App() {
                 <Route path="/" element={<UsersList />} />
                 <Route path="/create" element={<CreateUser />} />
                 <Route path="/edit/:id" element={<EditUser />} />
-                <Route path="/users/:id" element={<UserDetails />} /> {/* 2. Adicione esta nova rota */}
+                <Route path="/users/:id" element={<UserDetails />} />
             </Routes>
         </Router>
     );
